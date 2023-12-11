45016a8aa1e17f654f85b6600824c32ae804be32
kanglerian <kanglerian@gmail.com>
2023-12-11 19:10:32 +0700

    perbarui data

--- src/pages/Biodata.jsx
+++ src/pages/Biodata.jsx
@@ -763,7 +763,7 @@ const Biodata = () => {
 
             <button
               type="submit"
-              className="flex items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="flex justify-center items-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               {loading && <Loading width={5} height={5} fill="fill-sky-500" color="text-gray-200" />}
               Perbarui Data
