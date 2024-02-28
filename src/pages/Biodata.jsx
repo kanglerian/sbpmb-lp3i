8959731e335e56a487ef769e84f5b8d15ddd4e10
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-02-28 13:44:52 +0700

    tet to number

--- src/pages/Biodata.jsx
+++ src/pages/Biodata.jsx
@@ -469,7 +469,7 @@ const Biodata = () => {
                   No. Kartu Indonesia Pintar
                 </label>
                 <input
-                  type="number"
+                  type="text"
                   value={kip}
                   onChange={(e) => setKip(e.target.value)}
                   className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
