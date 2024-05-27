76a629e47eea03bbc9ecf838ae5bf7ec1e50e3d0
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-05-27 14:18:10 +0700

    base

--- src/main.jsx
+++ src/main.jsx
@@ -64,6 +64,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} base="/sbpmb-lp3i" />
   </React.StrictMode>,
 )
